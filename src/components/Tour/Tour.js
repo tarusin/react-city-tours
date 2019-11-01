9d0b18e10e03b99d815570ae53282e60553ffeed
tarusin <ilya.tarusin@mycredit.ua>
2019-11-01 17:57:26 +0200

    first commit

---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import './Tour.scss';
+
+class Tour extends Component {
+    state = {
+        showInfo: false
+    };
+
+    toggleInfo = () => {
+      this.setState({
+          showInfo: !this.state.showInfo
+      })
+    };
+
+    render() {
+        console.log('tour', this.props);
+        const {id, city, img, name, info} = this.props.tour;
+        const {removeTour} = this.props;
+
+        return (
+            <div className="tour">
+                <div className="img-container">
+                    <img src={img} alt=""/>
+                    <span className="close-btn"
+                          onClick={() => {
+                        removeTour(id)
+                    }}>
+                        <i className="fas fa-window-close"/>
+                    </span>
+                </div>
+                <div className="tour-info">
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
+                    <h5>
+                        info
+                        <span onClick={this.toggleInfo}>
+                            <i className="fas fa-caret-square-down"/>
+                        </span>
+                    </h5>
+                    {this.state.showInfo && <p>{info}</p>}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Tour;
